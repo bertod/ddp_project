fd7d4ba5b5da43aafaedfe9d287f4fbe675822cb
bdattoma@unibz.it <bertodattoma@gmail.com>
2020-01-04 17:28:25 +0100

    scraper class tests

---
+++ tests/test_scraper.py
@@ -0,0 +1,21 @@
+import pytest
+from ResearcherNetwork.scraper import Scraper
+
+
+class TestScraper:
+    def setup_method(self):
+        self.scraper = Scraper()
+
+    def test_get_html_from_url(self):
+        url = "https://dblp.org/db/conf/icml/icml2017"
+        response = self.scraper.get_html_from_url(url)
+        f = open("html_response.html", "r", encoding="utf8")
+        html_response = f.read()
+        assert response == html_response
+
+    # def test_scrape(self):
+    #     f = open("page_html_to_scrape.html", "r")
+    #     html_page = f.read()
+    #     #print(html_page)
+    #     self.scraper.scrape(target_tag='href', html_page=html_page)
+
